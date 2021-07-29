dc525674e17ab528384eb627200f1d9acb077428
Arman Abkar <armanabkar@gmail.com>
2021-07-29 10:54:26 +0430

    add suggestions functionality

--- components/Suggestion.tsx
+++ components/Suggestion.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Alert, Text, TouchableOpacity, Platform } from "react-native";
 import styles from "../assets/styles";
+import store from "../store";
 
-const Suggestion = () => (
-  <TouchableOpacity style={styles.suggestions}>
-    <Text style={styles.suggestionText}>Random Suggestion</Text>
-  </TouchableOpacity>
-);
+const Suggestion = () => {
+  const [suggestionLabel, setSuggestionLabel] =
+    useState<string>("Random Suggestion");
+
+  const createAlert = () => {
+    Alert.alert(
+      store.suggestions[Math.floor(Math.random() * store.suggestions.length)]
+    );
+    setSuggestionLabel(
+      store.suggestions[Math.floor(Math.random() * store.suggestions.length)]
+    );
+    setTimeout(() => {
+      setSuggestionLabel("Random Suggestion");
+    }, 2000);
+  };
+
+  return (
+    <TouchableOpacity style={styles.suggestions} onPress={createAlert}>
+      <Text style={styles.suggestionText}>
+        {Platform.OS === "web" ? suggestionLabel : "Random Suggestion"}
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
 export default Suggestion;
